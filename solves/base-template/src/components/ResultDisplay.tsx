df4fe536f22e4545bd1884d84a6d581961c1cb91
simshadows <contact@simshadows.com>
2022-08-02 08:58:46 +1000

    Created a dedicated solution parsing module

--- solves/base-template/src/components/ResultDisplay.tsx
+++ solves/base-template/src/components/ResultDisplay.tsx
@@ -1,73 +1,52 @@
 import React from "react";
 
-import {type ClingoResult} from "../runSolver";
+import {type ParsedSolutionSpecialCode} from "../runSolver";
 
 import "./ResultDisplay.css";
 
-const solutionRE = /solution\(color\(([^,]+),([^,]+)*\)\)/g;
-
-function parseSolutionStr(s: string) {
-    const matches = [...s.matchAll(solutionRE)];
-    if (matches.length !== 1) return null;
-    const match = matches[0];
-    if (match?.length !== 3) return null;
-    return {
-        vertex: match?.[1] || "<INVALID>",
-        colour: match?.[2] || "<INVALID>",
-    };
-}
-
-function renderRow(s: string, i: number) {
-    const parsed = parseSolutionStr(s);
-    if (parsed === null) return null;
+function renderRow(strs: string[], i: number) {
     return <tr key={i}>
-        <td>{parsed.vertex}</td>
-        <td>{parsed.colour}</td>
+        {strs.map((x: string, j: number) =>
+            <td key={j}>{x}</td>
+        )}
     </tr>;
-}
-
-function renderSatisfiable(solution: string[]) {
-    return <table className="result-satisfiable">
-        <thead>
-            <tr>
-                <th>Vertex</th>
-                <th>Colour</th>
-            </tr>
-        </thead>
-        <tbody>
-            {solution.map(renderRow)}
-        </tbody>
-    </table>;
 }
 
 /*** ***/
 
 interface Props {
-    initialized: boolean;
-    clingoResult: null | ClingoResult;
+    fieldLabels: string[];
+    solutionData: string[][] | ParsedSolutionSpecialCode | "not-initialized";
 }
 
 export function ResultDisplay(props: Props) {
-    if (!props.initialized) {
-        console.assert(props.clingoResult === null);
-        return <>Loading...</>;
-    } else if (props.clingoResult === null) {
-        return <>Invalid Input.</>;
+    switch (props.solutionData) {
+        case "not-initialized":
+            return <>Loading...</>;
+        case "no-solution":
+            return <>No solution.</>;
+        case "invalid-input":
+            return <>Invalid input.</>;
+        case "error":
+            return <>An error has occurred.</>;
+        default: // Fallthrough
+    }
+    if (props.solutionData.length === 0) {
+        return <>There is a solution, but it has no values.</>;
     }
 
-    switch (props.clingoResult.result) {
-        case "SATISFIABLE":
-            const solution = props.clingoResult.solution;
-            if (solution !== null && solution.length !== 0) {
-                return renderSatisfiable(solution);
-            } else {
-                return <>There is a solution, but it has no values.</>;
-            }
-        case "UNSATISFIABLE":
-            return <>No solution.</>;
-        default: console.error("Invalid value."); // Fallthrough
-        case "UNKNOWN":
-            return <>{props.clingoResult.stderr || "UNKNOWN" }</>;
-    }
+    console.log(props.solutionData);
+    return <table className="result-satisfiable">
+        <thead>
+            <tr key={0}>
+                {props.fieldLabels.map((x: string, i: number) =>
+                    <th key={i}>{x}</th>
+                )}
+            </tr>
+        </thead>
+        <tbody>
+            {props.solutionData.map(renderRow)}
+        </tbody>
+    </table>;
 }
 
