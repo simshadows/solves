ea89201c6e05c1f68d7460b242395b0082bdb064
simshadows <contact@simshadows.com>
2022-08-08 14:54:29 +1000

    Implemented unused numeric state

--- solves/src/code-generator/template-api.ts
+++ solves/src/code-generator/template-api.ts
@@ -10,16 +10,33 @@ import {type SpecValues} from "../spec-parse";
 //    // Nothing for now
 //}
 
+const independentAPI: {[key: string]: any} = {
+    upperCaseFirst: (s: string) => {
+        // Some type checks since this actually runs in an EJS environment
+        if (typeof s !== "string") {
+            throw new Error(`upperCaseFirst() expected a string. Instead got type ${typeof s}.`);
+        }
+
+        return s.slice(0,1).toUpperCase() + s.slice(1)
+    },
+}
+
 function getTemplateAPI(specValues: SpecValues): {[key: string]: any} {
     const ret: {[key: string]: any} = {
         ...specValues,
-        upperCaseFirst: (s: string) => {
-            // Some type checks since this actually runs in an EJS environment
-            if (typeof s !== "string") {
-                throw new Error(`upperCaseFirst() expected a string. Instead got type ${typeof s}.`);
-            }
+        ...independentAPI,
 
-            return s.slice(0,1).toUpperCase() + s.slice(1)
+        allInputs: {
+            ...specValues.inputBase,
+            //...specValues.inputInteger,
+            ...specValues.inputConstrained,
+        },
+        allTextInputs: {
+            ...specValues.inputBase,
+            ...specValues.inputConstrained,
+        },
+        allNumericInputs: {
+            ...specValues.inputInteger,
         },
     };
     return ret;
